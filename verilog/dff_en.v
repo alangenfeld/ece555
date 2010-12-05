7f60ee948bdbb232065c4be9583b4acf5c27d33d
James Allen Nystrom <jnystrom@tux-52.cae.wisc.edu>
2010-12-04 20:42:34 -0600

    dff_en plus more multipler_final work

--- verilog/dff_en.v
+++ verilog/dff_en.v
@@ -1,6 +1,6 @@
 // Library - ece555_final, Cell - dff_en, View - schematic
-// LAST TIME SAVED: Dec  4 18:38:54 2010
-// NETLIST TIME: Dec  4 18:39:47 2010
+// LAST TIME SAVED: Dec  4 20:00:45 2010
+// NETLIST TIME: Dec  4 20:02:15 2010
 `timescale 1ns / 1ns 
 
 module dff_en ( Q, D, clk, wr_en );
@@ -16,12 +16,12 @@ specify
     specparam CDS_VIEWNAME = "schematic";
 endspecify
 
-dff I8 ( Q, net12, D);
+dff I8 ( Q, net24, D);
 pmos P3 ( net042, 1, wr_en);
 pmos P0 ( net12, 1, clk);
-pmos P1 ( net12, 1, net042);
+pmos P1 ( net24, net12, net042);
 nmos N3 ( net042, 0, wr_en);
 nmos N1 ( net24, 0, net042);
-nmos N0 ( net12, net24, clk);
+nmos N0 ( net24, 0, clk);
 
 endmodule
